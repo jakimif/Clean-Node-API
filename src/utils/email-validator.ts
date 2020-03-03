81627b7c835956316eb971fae5def86f8aa6ba92
André França <avila.andre31@gmail.com>
2020-03-03 11:34:25 -0300

    temp: comment EmailValidatorAdapter

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -3,6 +3,7 @@ import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
+    //
     return validator.isEmail(email)
   }
 }
